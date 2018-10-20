463530b1d16f3c1c5c9f94a866b7af70f6539355
jisrag <i.s.r._182@hotmail.com>
2018-10-20 11:01:40 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3000;
+
+http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'textplain'});
+    res.end('Ahora si bro');
+}).listen(port, hostname, () => {
+    console.log('servidor corriendo en el puerto ${port}' + port);
+
+})
+
